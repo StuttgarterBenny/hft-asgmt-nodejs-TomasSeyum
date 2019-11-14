43ec87b45d888d2f9074da484b62e8eb5cc4a54a
Tomas Seyum <91seto1bif@hft-stuttgart.de>
2019-11-14 16:05:38 +0100

    maybe

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-[![Build Status](https://travis-ci.org/hft-stuttgart-ipr/hft-asgmt-nodejs-USERNAME.svg?branch=master)](https://travis-ci.org/hft-stuttgart-ipr/hft-asgmt-nodejs-USERNAME)
+[![Build Status](https://travis-ci.org/hft-stuttgart-ipr/hft-asgmt-nodejs-TomasSeyum.svg?branch=master)](https://travis-ci.org/hft-stuttgart-ipr/hft-asgmt-nodejs-TomasSeyum)
 
 # hft-asgmt-nodejs
 
--- index.js
+++ index.js
@@ -1,29 +1,49 @@
 const express = require('express');
-
+const sqlite3 = require('sqlite3').verbose();
+const bodyParser = require('body-parser');
+const db = new sqlite3.Database('./db/shoutbox.db');
 const port = process.env.PORT || 3000;
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.set('view engine', 'ejs');
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
+  console.log('GET /');
   const pageTitle = 'This is a title';
-  const anArrayOfData = ['This', 'is', 'something', 'to', 'loop'];
-
-
-  res.render('pages/index', {
-    title: pageTitle,
-    data: anArrayOfData
-  });
+  db.all('SELECT * FROM shouts', (err, rows) => {
+    res.render('pages/index', {
+      title: pageTitle,
+      data: rows
+    });
+  })
 });
 
-app.get('/add-entry', function(req, res) {
+app.get('/add-entry', function (req, res) {
+  console.log('GET /add-entry');
   res.render('pages/add-entry');
 });
 
+app.post('/add-entry', function (req, res) {
+  console.log('POST /add-entry');
+  console.log(req.body);
+  db.run('INSERT INTO shouts(username, message) VALUES (?, ?);',
+    [req.body.username, req.body.message],
+    (err) => {
+      if (err) {
+        console.log(err);
+        res.render('pages/add-entry');
+      } else {
+        res.redirect('/');
+      }
+    })
+});
+
 const server = app.listen(port, () => {
- console.log(`Server listening on port ${port}…`)
+  console.log(`Server listening on port ${port}…`)
 });
 
 module.exports = server;
